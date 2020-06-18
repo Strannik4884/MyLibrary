8ee0b598bce0aa41cbed0dfe2c5490bf3828914b
Strannik4884 <strannik4884@mail.ru>
2020-06-18 03:06:39 +0300

    Fix book form

--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -6,18 +6,37 @@ use App\Entity\Book;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class BookType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('author')
-            ->add('dateRead')
+            ->add('name', null, [
+                'constraints' => [
+                    new Length([
+                        'max' => 255
+                    ])
+                ],
+                'label' => 'Название',
+                'attr' => [
+                    'class' => 'validate'
+                ]
+            ])
+            ->add('author', null, [
+                'constraints' => [
+                    new Length([
+                        'max' => 255
+                    ])
+                ],
+                'label' => 'Автор',
+                'attr' => [
+                    'class' => 'validate'
+                ]
+            ])
             ->add('bookPhotoName')
             ->add('bookFileName')
-            ->add('createdBy')
         ;
     }
 
